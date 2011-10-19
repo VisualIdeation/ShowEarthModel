7304594c76a3a655d0b9ebad164f8cb1ce475f1e
Patrick O'Leary <olearypatrick@gmail.com>
2011-10-19 14:21:03 -0400

    Added a new set a scripts for demonstrating earthquakes separated by magnitude value.

---
+++ scripts/quake_sets-3D.sh
@@ -0,0 +1,1 @@
+./bin/ShowEarthModel -rootSection 3DTV -creditFile scripts/quakes.credit -quakes ../data/Earthquakes/quakes_3.txt ../data/Earthquakes/quakes_4.txt ../data/Earthquakes/quakes_5.txt ../data/Earthquakes/quakes_6.txt ../data/Earthquakes/quakes_7.txt ../data/Earthquakes/quakes_8.txt ../data/Earthquakes/quakes_9.txt
